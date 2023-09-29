7c20e7c59458be5a0bdb3ef26cd7a9119fc99594
Aryan0097 <aryansatani100@gmail.com>
2023-09-29 23:48:43 +0530

    new files are created

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authroutes = require('./routes/authRoutes');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://me2023:Atlasmongodb@cluster0.pfinqpn.mongodb.net/User?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+const dbURL = 'mongodb+srv://me2023:cFfbM2fVz5mwDxzh@cluster0.pfinqpn.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
+
+//   mongoose.connect(dbURL,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+//   .then(() => {
+//       console.log('Connected to The MongoDB')
+//   })
+//   .catch((error)=>{
+//       console.log(error)
+//   })
+  
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authroutes);
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
