4e4204d7bac475c706770058719ac384250d4a08
Aryan0097 <aryansatani100@gmail.com>
2023-10-05 22:38:22 +0530

    all complete. I learn how to authenticate user using node js,mongoDB,express.js,JSON web token,bcrypt,EJS etc..

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authroutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth,checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
-
+ 
 
 // view engine
 app.set('view engine', 'ejs');
@@ -31,6 +31,7 @@ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true})
   
 
 // routes
+app.get('*',checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authroutes);
--- tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
