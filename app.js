fca5d295738ff07069f46b255132e01e64ec0811
Aryan0097 <aryansatani100@gmail.com>
2023-10-05 21:12:17 +0530

    protecting route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authroutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -31,7 +32,7 @@ mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true})
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authroutes);
 
 
