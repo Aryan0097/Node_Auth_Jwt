7b13551c088d0c7cf799d00af37667e6f4db11e1
Aryan0097 <aryansatani100@gmail.com>
2023-10-05 17:51:17 +0530

    signup page starts working and new user can be create

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authroutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 
 // view engine
@@ -32,3 +34,24 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authroutes);
 
+
+
+//cookies
+app.get('/set-cookies',(req,res)=>{
+
+  // res.setHeader('Set-Cookie','newUser=true');
+
+  res.cookie('newUser',false);
+  // res.cookie('isEmployee',true,{maxAge:1000*5,secure:true});
+  // res.cookie('isEmployee',true,{maxAge:1000*5,httpOnly:true});
+
+
+  res.send('you got the cookies!');
+});
+
+app.get('/read-cookies',(req,res)=>{
+
+  const cookies=req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
